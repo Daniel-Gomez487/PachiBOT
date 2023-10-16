c004a2d17cab898d49639d486a9f6b3f02105a65
Daniel <danigomezlopez@gmail.com>
2023-10-16 13:12:06 +0200

    Update discord.py

--- discord.py
+++ discord.py
@@ -12,4 +12,4 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 # Below cogs represents our folder our cogs are in.
 initial_extensions = ['cogs.music']
 
-bot = commands.Bot(command_prefix=["!p ","k ","n "])
+bot = commands.Bot(command_prefix=["!p ","!k ","!n "])
