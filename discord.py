1b0825269362180c9c9a338195024249d16c2a27
Daniel <danigomezlopez@gmail.com>
2023-10-16 12:30:55 +0200

    Update discord.py

--- discord.py
+++ discord.py
@@ -1,1 +1,15 @@
-print("hello world")
+import os
+import sys, traceback
+
+import discord
+from discord.ext import commands
+from dotenv import load_dotenv
+
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+# Below cogs represents our folder our cogs are in.
+initial_extensions = ['cogs.music']
+
+bot = commands.Bot(command_prefix='!')
