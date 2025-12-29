10a043459e5827a7ea7164680ab2e36066810abf
Daniel <77883565+Daniel-Gomez487@users.noreply.github.com>
2025-12-29 19:19:36 +0100

    Asegurado token y cambiado una mamada

--- antiterciano.py
+++ antiterciano.py
@@ -1,7 +1,10 @@
 import discord
 from discord.ext import commands
 from discord import app_commands
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
 
 class Client(commands.Bot):
     async def on_ready(self):
@@ -18,8 +21,8 @@ client = Client(command_prefix="!", intents=intents)
 TERCIANO_ID = discord.Object(id=497657958591627274)  # 👈 ID del usuario objetivo
 
 
-@client.tree.command(name="Kick", description="Exiliar a Terciano de los canales de voz")
+@client.tree.command(name="kick", description="Exiliar a Terciano de los canales de voz")
 async def kickTerciano(interaction: discord.Interaction):
     await interaction.response.send_message()
 
-client.run('MTE2MzQwNDI5NzczNjgxNDU5Mg.GQamwk.MP8UdruDWOqNmdwizQRKqVPdAImDZddlngn8xg')
+client.run(os.getenv("DISCORD_TOKEN"))
